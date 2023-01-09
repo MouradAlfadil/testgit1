bc1cae4a2efec566d9dd3ebe15d01e2d90c824d4
MouradAlfadil <119465416+MouradAlfadil@users.noreply.github.com>
2023-01-09 10:57:11 -0500

    Add files via upload

---
+++ Day 1 Lecture/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string myName = null;
+Console.WriteLine(myName);
+string friend = "Josh Carolin";
+
+if (friend == "Family")
+{
+    myName = "Justin";
+}
+else if (friend == "Josh Carolin")
+{
+    myName = "Jeo";
+}
+Console.WriteLine(myName);
+
